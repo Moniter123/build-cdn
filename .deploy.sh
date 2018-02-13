d50d5b2db92f1f72eed4c96c249f988f6bd7b3a4
Janos Pasztor <janos@pasztor.at>
2018-02-13 21:11:01 +0100

    Working CDN config

--- .deploy.sh
+++ .deploy.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-NODES=$(dig +short all.cdn.opsbears.net)
-
-for NODE in ${NODES}; do
-    rsync -e "ssh -p 2222" -avz ./_site/ "ubuntu@${NODE}:/srv/www/pasztor.at/htdocs/"
+for NODE in $(dig +short all.cdn.opsbears.net); do
+    rsync -e "ssh" --rsync-path="sudo rsync" -avz --delete ./_site/ $NODE:/srv/www/pasztor.at/htdocs/
 done
