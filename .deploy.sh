1171f573805d8b301f3a7be416f464e4026e5757
Janos Pasztor <janos@pasztor.at>
2018-02-14 21:25:42 +0100

    Minor fixes

--- .deploy.sh
+++ .deploy.sh
@@ -3,5 +3,5 @@
 set -e
 
 for NODE in $(dig +short all.cdn.opsbears.net); do
-    rsync -e "ssh" --rsync-path="sudo rsync" -avz --delete ./_site/ $NODE:/srv/www/pasztor.at/htdocs/
+    rsync -e "ssh" --rsync-path="sudo rsync" -avz --delete ./_site/ $NODE:/srv/www/pasztor.at/htdocs/ &
 done
