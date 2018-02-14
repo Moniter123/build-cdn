3b98d7df7e6885122132eb033e313cbc24b3d043
Janos Pasztor <janos@pasztor.at>
2018-02-14 08:06:39 +0100

    Fixed metadata bug

--- .deploy.sh
+++ .deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 for NODE in $(dig +short all.cdn.opsbears.net); do
     rsync -e "ssh" --rsync-path="sudo rsync" -avz --delete ./_site/ $NODE:/srv/www/pasztor.at/htdocs/
 done
